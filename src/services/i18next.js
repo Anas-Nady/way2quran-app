e6d4f02eab10e48a56c45b24a03967107649a0e1
Anas Nady <eng.anas.nady@gmail.com>
2024-12-01 12:56:29 +0200

    Set dynamic language detection based on user's device locale using `expo-localization`

--- src/services/i18next.js
+++ src/services/i18next.js
@@ -2,21 +2,25 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import ar from "./../locales/ar.json";
 import en from "./../locales/en.json";
+import * as Localization from "expo-localization";
 
 const languageResources = {
   en: { translation: en },
   ar: { translation: ar },
 };
 
-export const toggleLanguage = (locale) => {
-  i18n.changeLanguage(locale);
+const getDefaultLanguage = () => {
+  const getLocales = Localization.getLocales();
+  const deviceLanguage = getLocales[0].languageCode;
+
+  return deviceLanguage === "ar" ? "ar" : "en";
 };
 
 export const getCurrentLanguage = () => i18n.language;
 
 const intiInfo = {
   compatibilityJSON: "v3",
-  lng: "ar",
+  lng: getDefaultLanguage(),
   fallbackLng: "ar",
   debug: false,
   resources: languageResources,
