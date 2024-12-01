8c3bc870302747fe1a463c463b14990798843c45
Anas Nady <eng.anas.nady@gmail.com>
2024-12-01 15:55:05 +0200

    Refactor About screen: Split into reusable components

--- src/screens/AboutUs.jsx
+++ src/screens/AboutUs.jsx
@@ -1,69 +1,25 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, ScrollView } from "react-native";
 import HeadingScreen from "./../components/HeadingScreen";
 import GoBackButton from "../components/ui/GoBackButton";
 import { useTranslate } from "../helpers/i18nHelper";
-import {
-  FontAwesome5,
-  FontAwesome6,
-  MaterialCommunityIcons,
-} from "@expo/vector-icons";
 import { socialMedia } from "../constants/socialMedia";
-import { useState } from "react";
-import { Linking } from "react-native";
-import Alert from "../components/ui/Alert";
-import { TouchableOpacity } from "react-native";
+import SocialMediaIcons from "../components/AboutUs/SocialMediaIcons";
 import { chunkArray } from "../helpers/chunkArray";
-import { flexDirection, isRTL } from "../helpers/flexDirection";
+import DescriptionList from "../components/AboutUs/DescriptionList";
 
 const AboutUsList = () => {
   const translate = useTranslate("AboutScreen");
-  const [error, setError] = useState("");
 
-  const handlePress = (url) => {
-    Linking.openURL(url);
-  };
-
-  const SocialMediaIcons = ({ socialMediaItems }) => {
-    const chunks = chunkArray(socialMediaItems, 5);
-    return (
-      <View>
-        {chunks.map((chunk, index) => (
-          <View
-            key={index}
-            className={`${flexDirection()} my-1 justify-between items-center p-4 w-[90%] mx-auto bg-gray-700 border border-gray-600`}
-          >
-            {chunk.map((item) => {
-              const IconComponent =
-                item.icon.includes("gmail") || item.icon.includes("web")
-                  ? MaterialCommunityIcons
-                  : item.icon === "threads" || item.icon === "square-x-twitter"
-                  ? FontAwesome6
-                  : FontAwesome5;
-
-              return (
-                <TouchableOpacity
-                  key={item.id}
-                  onPress={() => handlePress(item.url)}
-                >
-                  <IconComponent
-                    name={item.icon}
-                    size={24}
-                    color={item.color}
-                  />
-                </TouchableOpacity>
-              );
-            })}
-          </View>
-        ))}
-      </View>
-    );
-  };
+  const descriptionList = Array.from({ length: 3 }, (_, index) => ({
+    id: (index + 1).toString(),
+    text: translate(`about_${index + 1}`),
+  }));
 
   return (
     <ScrollView
       style={{ flex: 1 }}
       className="w-full bg-gray-800"
-      contentContainerStyle={{ flexGrow: 1, paddingBottom: 20 }}
+      contentContainerStyle={{ flexGrow: 1, gap: 8 }}
       showsVerticalScrollIndicator={false}
       nestedScrollEnabled={true}
     >
@@ -71,27 +27,8 @@ const AboutUsList = () => {
         <GoBackButton />
         <HeadingScreen headingTxt={translate("aboutTitle")} />
       </View>
-
-      {error && (
-        <Alert message={error} onClose={() => setError("")} type="error" />
-      )}
-
-      {Array.from({ length: 3 }, (_, index) => (
-        <View
-          key={index + 1}
-          className="p-2.5 my-1.5 w-[90%] mx-auto bg-gray-700 border border-gray-600 rounded"
-        >
-          <Text
-            className={`${
-              isRTL ? "text-[20px]" : "text-[17px]"
-            } font-medium leading-normal text-center text-green-500`}
-          >
-            {translate(`about_${index + 1}`)}
-          </Text>
-        </View>
-      ))}
-
-      <SocialMediaIcons socialMediaItems={socialMedia} />
+      <DescriptionList data={descriptionList} />
+      <SocialMediaIcons data={chunkArray(socialMedia, 5)} />
     </ScrollView>
   );
 };
