16231a3f28f2f992ab1a96f56b7dc5d558740d2e
Anas Nady <eng.anas.nady@gmail.com>
2024-12-01 15:15:53 +0200

    Replace SafeAreaView from 'react-native' with SafeAreaView from 'react-native-safe-area-context`

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,4 @@
-import {
-  SafeAreaView,
-  StatusBar,
-  View,
-  TouchableWithoutFeedback,
-} from "react-native";
+import { StatusBar, View, TouchableWithoutFeedback } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import TabBar from "../components/Navigation/TabBar";
 import AudioPlayerModal from "../components/Reciter/AudioPlayerModal";
@@ -12,6 +7,7 @@ import { useState, useCallback, useEffect } from "react";
 import { currentLanguage } from "../helpers/flexDirection";
 import SplashScreen from "../screens/SplashScreen";
 import TopBar from "./Navigation/TopBar";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 function getPlayerModalHeight(playerState) {
   if (playerState.isModalVisible) {
@@ -46,51 +42,54 @@ function Layout({ playerState }) {
   }
 
   return (
-    <NavigationContainer independent={true}>
-      <SafeAreaView
-        className={`${
-          currentLanguage === "ar" ? "font-arabic" : "font-english"
-        } flex-1 bg-gray-800`}
-      >
-        <StatusBar backgroundColor="#22c55e" barStyle="light-content" />
+    <SafeAreaProvider>
+      <NavigationContainer independent={true}>
+        <SafeAreaView
+          style={{ flex: 1 }}
+          className={`${
+            currentLanguage === "ar" ? "font-arabic" : "font-english"
+          } flex-1 bg-gray-800`}
+        >
+          <StatusBar backgroundColor="#22c55e" barStyle="light-content" />
 
-        <TouchableWithoutFeedback onPress={closeMenu}>
-          <View style={{ flex: 1 }}>
-            <TopBar isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
-            <View
-              style={{
-                flex: 1,
-                marginBottom: playerModalHeight + tabsHeight,
-                paddingBottom: 5,
-              }}
-            >
-              <AppNavigator />
-            </View>
+          <TouchableWithoutFeedback onPress={closeMenu}>
+            <View style={{ flex: 1 }}>
+              <TopBar isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
+              <View
+                style={{
+                  flex: 1,
+                  marginBottom: playerModalHeight + tabsHeight,
+                  paddingBottom: 5,
+                }}
+              >
+                <AppNavigator />
+              </View>
 
-            {/* Audio Player Modal */}
-            <View
-              style={{
-                position: "absolute",
-                bottom: tabsHeight + 5,
-                left: 0,
-                right: 0,
-                zIndex: 200,
-              }}
-              className="w-full"
-            >
-              <AudioPlayerModal />
-            </View>
+              {/* Audio Player Modal */}
+              <View
+                style={{
+                  position: "absolute",
+                  bottom: tabsHeight + 5,
+                  left: 0,
+                  right: 0,
+                  zIndex: 200,
+                }}
+                className="w-full"
+              >
+                <AudioPlayerModal />
+              </View>
 
-            {/* Tab Bar */}
-            <View
-              style={{ position: "absolute", bottom: 0, left: 0, right: 0 }}
-            >
-              <TabBar closeMenu={closeMenu} />
+              {/* Tab Bar */}
+              <View
+                style={{ position: "absolute", bottom: 0, left: 0, right: 0 }}
+              >
+                <TabBar closeMenu={closeMenu} />
+              </View>
             </View>
-          </View>
-        </TouchableWithoutFeedback>
-      </SafeAreaView>
-    </NavigationContainer>
+          </TouchableWithoutFeedback>
+        </SafeAreaView>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
