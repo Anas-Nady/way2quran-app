782c4506dc1e2174a27d335b546b1f169877c1fc
Anas Nady <eng.anas.nady@gmail.com>
2024-12-03 06:32:06 +0200

    remove `setupTrackPlayer` initialization from trackPlayer service and App component

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,25 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import TrackPlayer from "react-native-track-player";
 import { AudioPlayerProvider } from "../contexts/AudioPlayerContext";
 import RootScreenContent from "../screens/root";
-import trackPlayer, { setupTrackPlayer } from "../services/trackPlayer";
+import trackPlayer from "../services/trackPlayer";
 
 function App() {
-  const [isPlayerReady, setIsPlayerReady] = useState(false);
-
-  useEffect(() => {
-    const initializeTrackPlayer = async () => {
-      const setup = await setupTrackPlayer();
-      setIsPlayerReady(setup);
-    };
-
-    initializeTrackPlayer();
-  }, []);
-
-  if (!isPlayerReady) {
-    return null;
-  }
-
   return (
     <AudioPlayerProvider>
       <RootScreenContent />
--- src/services/trackPlayer.js
+++ src/services/trackPlayer.js
@@ -3,22 +3,6 @@ import TrackPlayer, {
   Capability,
 } from "react-native-track-player";
 
-export const setupTrackPlayer = async () => {
-  try {
-    const isInitialized = await TrackPlayer.isServiceRunning();
-    if (!isInitialized) {
-      await TrackPlayer.setupPlayer({
-        autoHandleInterruptions: true,
-      });
-      console.log("Track Player is set up");
-    }
-    return true;
-  } catch (error) {
-    console.error("Error setting up Track Player:", error);
-    return false;
-  }
-};
-
 export default async function () {
   TrackPlayer.updateOptions({
     capabilities: [
