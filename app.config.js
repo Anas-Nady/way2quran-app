558351391185a6d57b0abc3a6faac0721a948c03
Anas Nady <eng.anas.nady@gmail.com>
2024-12-28 09:50:55 +0200

    Version updated to 1.0.8

--- app.config.js
+++ app.config.js
@@ -48,7 +48,7 @@ export default () => {
   };
 
   const commonConfig = {
-    version: "1.0.7",
+    version: "1.0.8",
     orientation: "default",
     sdkVersion: "51.0.0",
     icon: "./src/assets/images/ios-icon.png",
