d1363ae3a0adeab03537aed343c77a4a5e47e2ea
Anas Nady <eng.anas.nady@gmail.com>
2025-01-29 01:55:32 +0200

    feat: display app version on custom Splash Screen using expo-constants

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import Constants from "expo-constants";
+import { useTranslation } from "react-i18next";
 
 const SplashScreen = () => {
+  const appVersion = Constants.expoConfig.version;
+  const { t } = useTranslation("");
+
   return (
     <View style={styles.container}>
       <Image
@@ -9,6 +14,15 @@ const SplashScreen = () => {
         resizeMode="contain"
         style={{ width: "100%", height: "100%", flex: 1 }}
       />
+      <Text
+        className="text-xl font-bold text-green-600 "
+        style={{
+          position: "absolute",
+          bottom: 35,
+        }}
+      >
+        {t("version")}: {appVersion}
+      </Text>
     </View>
   );
 };
