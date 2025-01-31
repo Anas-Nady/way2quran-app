175732d48e64ccfcd64ef6f7010e1fc1cf8aef41
Anas Nady <eng.anas.nady@gmail.com>
2025-01-31 15:40:30 +0200

    Refactor SplashScreen: display only version number and remove text

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 import Constants from "expo-constants";
-import { useTranslation } from "react-i18next";
 
 const SplashScreen = () => {
   const appVersion = Constants.expoConfig.version;
-  const { t } = useTranslation("");
 
   return (
     <View style={styles.container}>
@@ -15,13 +13,13 @@ const SplashScreen = () => {
         style={{ width: "100%", height: "100%", flex: 1 }}
       />
       <Text
-        className="text-xl font-bold text-green-600 "
+        className="text-lg font-bold text-green-600"
         style={{
           position: "absolute",
-          bottom: 35,
+          bottom: 30,
         }}
       >
-        {t("version")}: {appVersion}
+        v{appVersion}
       </Text>
     </View>
   );
