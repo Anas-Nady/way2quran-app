86fe286ca1cc086da950db9661a9df863e43aac9
Anas Nady <eng.anas.nady@gmail.com>
2025-02-08 06:54:23 +0200

    Update version to 1.1.15

--- app.config.js
+++ app.config.js
@@ -48,7 +48,7 @@ export default () => {
   };
 
   const commonConfig = {
-    version: "1.0.15",
+    version: "1.1.15",
     orientation: "default",
     sdkVersion: "51.0.0",
     icon: "./src/assets/images/ios-icon.png",
