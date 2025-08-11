187b53e65961703b561696948b320224bc10a7c5
Anas Nady <eng.anas.nady@gmail.com>
2025-08-11 16:18:56 +0300

    Remove `flexDirection` & `rowDirection`

--- src/helpers/flexDirection.ts
+++ src/helpers/flexDirection.ts
@@ -2,16 +2,6 @@ import { appLanguage } from "../services/i18next";
 
 export const isRTL = appLanguage === "ar";
 
-export const flexDirection = () => {
-  if (isRTL) return "flex-row";
-  else return "flex-row";
-};
-
-export const rowDirection = () => {
-  if (isRTL) return "row";
-  else return "row";
-};
-
 export const textDirection = () => {
   return isRTL ? "text-right" : "text-left";
 };
